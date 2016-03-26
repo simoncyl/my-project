354b227402acb2aba5bcd355a20c3ec61fe551cd
simoncyl <simon.cyl@gmail.com>
2016-03-27 07:13:16 +0800

    my first object-c practice
    for object-c practice noted

---
+++ MyObjectCHW/MyObjectCHW/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyObjectCHW
+//
+//  Created by simon on 2016/3/27.
+//  Copyright © 2016年 simon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
