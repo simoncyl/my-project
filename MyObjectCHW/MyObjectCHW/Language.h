e5f503c0af250582947170ed36da8cc07105b414
simoncyl <simon.cyl@gmail.com>
2016-03-27 12:03:52 +0800

    Add Language clad

---
+++ MyObjectCHW/MyObjectCHW/Language.h
@@ -0,0 +1,14 @@
+//
+//  Language.h
+//  MyObjectCHW
+//
+//  Created by simon on 2016/3/27.
+//  Copyright © 2016年 simon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Language : NSObject
+
+
+@end
---
+++ MyObjectCHW/MyObjectCHW/Language.m
@@ -0,0 +1,13 @@
+//
+//  Language.m
+//  MyObjectCHW
+//
+//  Created by simon on 2016/3/27.
+//  Copyright © 2016年 simon. All rights reserved.
+//
+
+#import "Language.h"
+
+@implementation Language
+
+@end
